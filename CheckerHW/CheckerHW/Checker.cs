c78dc6abccecdd67ce6ef1ed074779ca4e90400e
camjam21167 <csiebert8910@gmail.com>
2023-02-07 12:47:38 -0600

    Password Checker HW Finished

---
+++ CheckerHW/CheckerHW/Checker.cs
@@ -0,0 +1,141 @@
+﻿using System;
+namespace CheckerHW
+{
+	public class Checker
+	{
+		string inPassword;
+		public Checker(string inPassword)
+		{
+			this.inPassword = inPassword;
+		}
+		public string checkPassword()
+		{
+			string retString = "0";
+			if (inPassword.Length < 8)
+			{
+				retString = "1";
+			}
+			if (checkForUpper() == false)
+			{
+				if(retString.Equals("0"))
+				{
+					retString = "2";
+				}
+				else
+				{
+					retString += ",2";
+				}
+			}
+			if (checkForLower() ==  false)
+			{
+				if(retString.Equals("0"))
+				{
+					retString = "3";
+				}
+				else
+				{
+					retString += ",3";
+				}
+			}
+			if (checkForChars() == false)
+			{
+				if(retString.Equals("0"))
+				{
+					retString = "4";
+				}
+				else
+				{
+					retString += ",4";
+				}
+			}
+			if (checkForDigits() == false)
+			{
+				if(retString.Equals("0"))
+				{
+					retString = "5";
+				}
+				else
+				{
+					retString += ",5";
+				}
+			}
+			if (checkForWords() == false)
+			{
+				if(retString.Equals("0"))
+				{
+					retString = "6";
+				}
+				else
+				{
+					retString += ",6";
+				}
+			}
+			return retString;
+		}
+
+        private bool checkForWords()
+        {
+			if (!(inPassword.Contains("max") || inPassword.Contains("bank") || inPassword.Contains("money") || inPassword.Contains("cash")))
+			{
+				return true;
+			}
+			return false;
+        }
+
+        private bool checkForDigits()
+        {
+			int total = 0;
+            foreach (char character in inPassword)
+			{
+				if (Char.IsDigit(character))
+				{
+					total++;
+				}
+			}
+			if(total >= 2)
+			{
+				return true;
+			}
+			return false;
+        }
+
+        private bool checkForChars()
+        {
+            foreach (char character in inPassword)
+			{
+				if (character.Equals('*') || character.Equals('&') || character.Equals('^') || character.Equals('!') || character.Equals('$'))
+				{
+					if (!inPassword.Last().Equals(character))
+					{
+						return true;
+					}
+				}
+			}
+			return false;
+        }
+
+        private bool checkForLower()
+        {
+			foreach (char letter in inPassword)
+			{
+				if (Char.IsLower(letter))
+				{
+					return true;
+				}
+			}
+			return false;
+        }
+
+        private bool checkForUpper()
+        {
+			foreach(char letter in inPassword)
+			{
+				if (Char.IsUpper(letter))
+				{
+					return true;
+				}
+			}
+			return false;
+        }
+    }
+}
---
+++ CheckerHW/CheckerTests/UnitTest1.cs
@@ -0,0 +1,85 @@
+﻿using CheckerHW;
+
+namespace CheckerTests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestLengthRequirement()
+    {
+        string password = "i0$2J";
+        Checker check = new Checker(password);
+        Assert.AreEqual("1", check.checkPassword());
+    }
+    [TestMethod]
+    public void TestUpperRequirement()
+    {
+        string password = "ifi*u2bi6jbwdi";
+        Checker check = new Checker(password);
+        Assert.AreEqual("2", check.checkPassword());
+    }
+    [TestMethod]
+    public void TestLowerRequirement()
+    {
+        string password = "I2BFJ8IEB&FJON";
+        Checker check = new Checker(password);
+        Assert.AreEqual("3", check.checkPassword());
+    }
+    [TestMethod]
+    public void TestSpecialCharacterRequirement()
+    {
+        string password = "IdF2Jfr4EBFJO";
+        Checker check = new Checker(password);
+        Assert.AreEqual("4", check.checkPassword());
+    }
+    [TestMethod]
+    public void TestSpecialCharacterRequirement2()
+    {
+        string password = "IdFJ1frEB5FJO*";
+        Checker check = new Checker(password);
+        Assert.AreEqual("4", check.checkPassword());
+    }
+    [TestMethod]
+    public void TestDigitRequirement()
+    {
+        string password = "Id$FJfrEBFJO";
+        Checker check = new Checker(password);
+        Assert.AreEqual("5", check.checkPassword());
+    }
+    [TestMethod]
+    public void TestDigitRequirementWorks()
+    {
+        string password = "Id$F9Jf5rEBFJO";
+        Checker check = new Checker(password);
+        Assert.AreEqual("0", check.checkPassword());
+    }
+    [TestMethod]
+    public void TestWordRequirement()
+    {
+        string password = "Id$F9Jbankf5rEBFJO";
+        Checker check = new Checker(password);
+        Assert.AreEqual("6", check.checkPassword());
+    }
+    [TestMethod]
+    public void TestWordRequirementWorks()
+    {
+        string password = "Id$F9Jbnkf5rEBFJO";
+        Checker check = new Checker(password);
+        Assert.AreEqual("0", check.checkPassword());
+    }
+    [TestMethod]
+    public void TestMultipleRequirements()
+    {
+        string password = "d9bankf";
+        Checker check = new Checker(password);
+        Assert.AreEqual("1,2,4,5,6", check.checkPassword());
+    }
+    [TestMethod]
+    public void TestLengthLowerRequirements()
+    {
+        string password = "I9V&4I";
+        Checker check = new Checker(password);
+        Assert.AreEqual("1,3", check.checkPassword());
+    }
+}
