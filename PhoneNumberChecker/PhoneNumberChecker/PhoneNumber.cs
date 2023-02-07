91aff318135792eb29b7f9ac64ac6b9d1baa880d
camjam21167 <csiebert8910@gmail.com>
2023-02-07 14:56:06 -0600

    InClass 2-7

---
+++ PhoneNumberChecker/PhoneNumberChecker/PhoneNumber.cs
@@ -0,0 +1,89 @@
+﻿using System;
+namespace PhoneNumberChecker
+{
+	public class PhoneNumber
+	{
+		private string phoneNumber;
+
+        public PhoneNumber(string phoneNumber)
+		{
+			this.phoneNumber = phoneNumber;
+		}
+		public bool isValidPhone()
+		{
+			if(phoneNumber.Length == 12)
+			{
+				if (isValid12Length())
+				{
+					if (!justDigits())
+					{
+						return false;
+					}
+				}
+				if (!validAreaCode())
+				{
+					return false;
+				}
+			}
+			if (isValidLength())
+			{
+				return true;
+			}
+			return false;
+		}
+
+        public bool validAreaCode()
+        {
+			char digit1 = phoneNumber[0];
+			if(digit1 == '0' || digit1 == '1')
+			{
+				return false;
+			}
+			return true;
+        }
+
+        public bool justDigits()
+        {
+			string pNum = phoneNumber.Replace("-", "");
+			if(pNum.Length != 10)
+			{
+				return false;
+			}
+			foreach(char c in pNum)
+			{
+				if (!char.IsDigit(c))
+				{
+					return false;
+				}
+			}
+			return true;
+        }
+
+        public bool isValid12Length()
+        {
+			int count = 0;
+			foreach(char c in phoneNumber)
+			{
+				if(count == 3 || count == 7)
+				{
+					if( c != '-')
+					{
+						return true;
+					}
+				}
+				count++;
+			}
+			return false;
+        }
+
+        private bool isValidLength()
+        {
+            string pNum = phoneNumber.Replace("-", "");
+            if (pNum.Length == 7 || pNum.Length == 10)
+			{
+				return true;
+			}
+			return false;
+        }
+    }
+}
---
+++ PhoneNumberChecker/PhoneNumberTests/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿namespace PhoneNumberTests;
+using PhoneNumberChecker;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestValidLength10Digit()
+    {
+        string pNum = "555-121-2222";
+        PhoneNumber pn = new PhoneNumber(pNum);
+        Assert.IsTrue(pn.isValidPhone());
+    }
+    [TestMethod]
+    public void TestValidLength7Digit()
+    {
+        string pNum = "121-2222";
+        PhoneNumber pn = new PhoneNumber(pNum);
+        Assert.IsTrue(pn.isValidPhone());
+    }
+    [TestMethod]
+    public void TestValid12Length()
+    {
+        string pNum = "555-121-2222";
+        PhoneNumber pn = new PhoneNumber(pNum);
+        Assert.IsFalse(pn.isValid12Length());
+    }
+    [TestMethod]
+    public void TestNotValid12Length()
+    {
+        string pNum = "555-121x2222";
+        PhoneNumber pn = new PhoneNumber(pNum);
+        Assert.IsTrue(pn.isValid12Length());
+    }
+    [TestMethod]
+    public void TestJustDigitsWDash()
+    {
+        string pNum = "555-121-2222";
+        PhoneNumber pn = new PhoneNumber(pNum);
+        Assert.IsTrue(pn.justDigits());
+    }
+    [TestMethod]
+    public void TestJustDigitsWDashBad()
+    {
+        string pNum = "555-121-22-2";
+        PhoneNumber pn = new PhoneNumber(pNum);
+        Assert.IsFalse(pn.justDigits());
+    }
+    [TestMethod]
+    public void ProveProfWrong()
+    {
+        string pNum = "555-121-b222";
+        PhoneNumber pn = new PhoneNumber(pNum);
+        Assert.IsTrue(pn.isValidPhone());
+    }
+}
